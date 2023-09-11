8336490bf9e52d272c2c39b2a20ef73f946b8e84
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-11 22:47:11 +0700

    add price

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { BsCart3 } from "react-icons/bs";
 
 const CardProducts = ({ product }) => {
   return (
@@ -8,6 +9,15 @@ const CardProducts = ({ product }) => {
       <Card.Body>
         <Card.Title className="">{product.title.substring(0, 50)}</Card.Title>
         <Card.Text>{product.description.substring(0, 100)}</Card.Text>
+        <div className="d-flex justify-content-between ">
+          <Button className="align-items-center">
+            <BsCart3 className="align-items-center me-1" />
+            <span>Add to Cart</span>
+          </Button>
+          <Button className="align-items-center bg-transparent text-black border-0">
+            <span className="justify-content-center"> $ {product.price} </span>
+          </Button>
+        </div>
       </Card.Body>
     </Card>
   );
