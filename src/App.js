181f77f534fa42edb1bec32dba21917ce1a35cea
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-11 23:40:47 +0700

    add cart page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Cart />} />
       </Routes>
     </Router>
   );
