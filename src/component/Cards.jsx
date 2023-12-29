aaf9a4b990df7be5624d26ff3d707d31127c49e5
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-12-29 20:05:06 +0700

    refactor : styling cards

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,11 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Typography,
-} from "@material-tailwind/react";
 
 export function Cards() {
   const [products, setProducts] = useState([]);
@@ -20,19 +14,27 @@ export function Cards() {
   }, []);
 
   return (
-    <div className="grid gap-3 mb-8 md:grid-cols-2 lg:grid-cols-4">
+    <div className="grid mb-3 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {products.map((item) => (
-        <Card key={item.id} className="mt-6 w-96 border border-blue-500">
-          <CardHeader className="my-4">
-            <img src={item.image} alt="card-image" className="w-1/2 mx-auto" />
-          </CardHeader>
-          <CardBody>
-            <Typography variant="h5" color="blue-gray" className="mb-2">
-              {item.title}
-            </Typography>
-            <Typography>{item.description}</Typography>
-          </CardBody>
-        </Card>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img class="h-48 w-50 mx-auto object-cover" src={item.image} />
+          <div class="px-6 py-4">
+            <h1 class="font-bold text-xl mb-2">
+              {item.title.substring(0, 20)}
+            </h1>
+            <p class="text-gray-700 text-base">
+              {item.description.substring(0, 150)}...
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2 flex justify-around">
+            <button class="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
+              Buy
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
+              Details
+            </button>
+          </div>
+        </div>
       ))}
     </div>
   );
