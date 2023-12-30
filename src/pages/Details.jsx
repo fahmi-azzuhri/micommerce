802db0063a8bbf4c17649dfb82cbe04f6b6cece1
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-12-30 21:46:09 +0700

    add containner setting

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -18,12 +18,14 @@ const Details = () => {
   });
 
   return (
-    <div className="grid lg:grid-cols-2 md:grid-cols-1">
-      <Toaster position="top-right" reverseOrder={false} />
-      <img className="w-1/5" src={details.image} alt="" />
-      <div className="flex flex-col">
-        <h1 className="text-3xl font-bold mb-4">{details.title}</h1>
-        <p className="text-xl">{details.description}</p>
+    <div className="container">
+      <div className="grid lg:grid-cols-2 md:grid-cols-1">
+        <Toaster position="top-right" reverseOrder={false} />
+        <img className="w-1/5" src={details.image} alt="" />
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold mb-4">{details.title}</h1>
+          <p className="text-xl">{details.description}</p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {},
   },
   plugins: [],
