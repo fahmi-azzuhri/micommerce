da58f2df8f614b441e60bc19cc2a6a74955ce27f
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-01-01 00:51:51 +0700

    add register page and change button color login page

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Details from "../pages/Details";
 import { Navbars } from "./Navbars";
 import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<Details />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,34 @@
-import { Card, Input, Typography, Button } from "@material-tailwind/react";
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 import { Link } from "react-router-dom";
-export function Login() {
+
+export function Register() {
   return (
     <Card color="transparent" shadow={false} className="items-center">
       <Typography variant="h4" color="blue-gray">
-        Login
+        Sign Up
       </Typography>
       <Typography color="gray" className="mt-1 font-normal">
-        Welcome back, please enter your details.
+        Nice to meet you! Enter your details to register.
       </Typography>
       <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
         <div className="mb-1 flex flex-col gap-6">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Name
+          </Typography>
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
           <Typography variant="h6" color="blue-gray" className="-mb-3">
             Your Email
           </Typography>
@@ -35,13 +53,31 @@ export function Login() {
             }}
           />
         </div>
+        <Checkbox
+          label={
+            <Typography
+              variant="small"
+              color="gray"
+              className="flex items-center font-normal"
+            >
+              I agree the
+              <a
+                href="#"
+                className="font-medium transition-colors hover:text-gray-900"
+              >
+                &nbsp;Terms and Conditions
+              </a>
+            </Typography>
+          }
+          containerProps={{ className: "-ml-2.5" }}
+        />
         <Button className="mt-6 bg-blue-500 hover:bg-gray-600" fullWidth>
-          sign in
+          sign up
         </Button>
         <Typography color="gray" className="mt-4 text-center font-normal">
-          Dont have an account? {""}
-          <Link className="text-gray-800 hover:text-blue-500" to="/register">
-            Sign up
+          Already have an account?{" "}
+          <Link className="text-gray-800 hover:text-blue-500" to="/login">
+            Sign in
           </Link>
         </Typography>
       </form>
