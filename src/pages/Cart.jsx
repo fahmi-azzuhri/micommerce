698d826147f14f82f867d2d80a23ffcda7e22a6e
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-01-07 14:50:39 +0700

    consume api cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,42 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 const Cart = () => {
+  const [carts, setCarts] = useState(
+    JSON.parse(localStorage.getItem("cart")) || []
+  );
+  const handleQuantityChange = (id, change) => {
+    const updatedCart = carts.map((item) => {
+      if (item.id === id) {
+        const newQuantity = item.quantity + change;
+        if (newQuantity <= 0) {
+          return null;
+        }
+
+        return { ...item, quantity: newQuantity };
+      }
+
+      return item;
+    });
+    const filteredCart = updatedCart.filter((item) => item !== null);
+
+    setCarts(filteredCart);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(carts));
+  }, [carts]);
+
+  const handleRemoveItem = (id) => {
+    const updatedCart = carts.filter((item) => item.id !== id);
+    setCarts(updatedCart);
+  };
   return (
     <div class="container mx-auto mt-10">
       <div class="flex shadow-md my-10">
         <div class="w-3/4 bg-white px-10 py-10">
           <div class="flex justify-between border-b pb-8">
             <h1 class="font-semibold text-2xl">Shopping Cart</h1>
-            <h2 class="font-semibold text-2xl">1 Items</h2>
+            <h2 class="font-semibold text-2xl"> {carts.length} Items</h2>
           </div>
           <div class="flex mt-10 mb-5">
             <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">
@@ -23,46 +52,62 @@ const Cart = () => {
               Total
             </h3>
           </div>
-          <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-            <div class="flex w-2/5">
-              <div class="w-20">
-                <img
-                  class="h-24"
-                  src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
-                  alt=""
-                />
+          {carts.map((cart) => (
+            <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+              <div class="flex w-2/5">
+                <div class="w-20">
+                  <img class="h-24" src={cart.image} alt="" />
+                </div>
+                <div class="flex flex-col justify-between ml-4 flex-grow">
+                  <span class="font-bold text-sm"> {cart.title} </span>
+                  <span className="text-red-500 text-xs">
+                    {" "}
+                    {cart.category}{" "}
+                  </span>
+                  <a
+                    onClick={() => handleRemoveItem(cart.id)}
+                    class="font-semibold hover:text-red-500 text-gray-500 text-xs cursor-pointer"
+                  >
+                    Remove
+                  </a>
+                </div>
               </div>
-              <div class="flex flex-col justify-between ml-4 flex-grow">
-                <span class="font-bold text-sm">Iphone 6S</span>
-                <span class="text-red-500 text-xs">Apple</span>
-                <a
-                  href="#"
-                  class="font-semibold hover:text-red-500 text-gray-500 text-xs"
+              <div class="flex justify-center w-1/5">
+                <svg
+                  onClick={() => handleQuantityChange(cart.id, -1)}
+                  className="fill-current text-gray-600 w-3 cursor-pointer"
+                  viewBox="0 0 448 512"
                 >
-                  Remove
-                </a>
+                  <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                </svg>
+
+                <input
+                  class="mx-2 border text-center w-8"
+                  type="text"
+                  value={cart.quantity}
+                />
+
+                <svg
+                  onClick={() => handleQuantityChange(cart.id, +1)}
+                  className="fill-current text-gray-600 w-3 cursor-pointer"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                </svg>
               </div>
+              <span class="text-center w-1/5 font-semibold text-sm">
+                ${cart.price}
+              </span>
+              <span class="text-center w-1/5 font-semibold text-sm">
+                ${cart.price * cart.quantity}
+              </span>
             </div>
-            <div class="flex justify-center w-1/5">
-              <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-                <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-              </svg>
+          ))}
 
-              <input
-                class="mx-2 border text-center w-8"
-                type="text"
-                value="1"
-              />
-
-              <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-                <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-              </svg>
-            </div>
-            <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
-            <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
-          </div>
-
-          <a href="#" class="flex font-semibold text-indigo-600 text-sm mt-10">
+          <Link
+            to="/"
+            class="flex font-semibold text-indigo-600 text-sm mt-10 cursor-pointer"
+          >
             <svg
               class="fill-current mr-2 text-indigo-600 w-4"
               viewBox="0 0 448 512"
@@ -70,14 +115,18 @@ const Cart = () => {
               <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
             </svg>
             Continue Shopping
-          </a>
+          </Link>
         </div>
 
         <div id="summary" class="w-1/4 px-8 py-10">
           <h1 class="font-semibold text-2xl border-b pb-8">Order Summary</h1>
           <div class="flex justify-between mt-10 mb-5">
-            <span class="font-semibold text-sm uppercase">Items 3</span>
-            <span class="font-semibold text-sm">590$</span>
+            <span class="font-semibold text-sm uppercase">
+              Items {`(${carts.length})`}
+            </span>
+            <span class="font-semibold text-sm">
+              ${carts.reduce((a, b) => a + b.price * b.quantity, 0).toFixed(2)}
+            </span>
           </div>
           <div>
             <label class="font-medium inline-block mb-3 text-sm uppercase">
@@ -107,7 +156,10 @@ const Cart = () => {
           <div class="border-t mt-8">
             <div class="flex font-semibold justify-between py-6 text-sm uppercase">
               <span>Total cost</span>
-              <span>$600</span>
+              <span>
+                $
+                {carts.reduce((a, b) => a + b.price * b.quantity, 0).toFixed(2)}
+              </span>
             </div>
             <button class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
               Checkout
