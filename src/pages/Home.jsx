0da39751c777a55a72586b5f2cb4cb9dce19935e
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-01-12 16:48:40 +0700

    fix footer double

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Cards } from "../component/Cards";
 import Hero from "../component/Hero";
-import { Footers } from "../component/Footers";
+
 const Home = () => {
   return (
     <div>
       <Hero />
       <Cards />
-      <Footers />
     </div>
   );
 };
