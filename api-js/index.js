8763c7548513f21d5947f4eb0034ecbfd5f782b4
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-02-09 23:32:46 +0700

    feat : add cors

--- api-js/index.js
+++ api-js/index.js
@@ -1,7 +1,14 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://micommerce.vercel.app"],
+  })
+);
 
 const users = [
   {
